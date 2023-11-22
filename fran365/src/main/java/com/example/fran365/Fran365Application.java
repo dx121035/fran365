25297c0e7d17e5891e1dc32f811870bf7f505323
dx121035 <dx121034@gmail.com>
2023-11-22 11:14:14 +0900

    add project

---
+++ fran365/src/main/java/com/example/fran365/Fran365Application.java
@@ -0,0 +1,13 @@
+package com.example.fran365;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fran365Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fran365Application.class, args);
+	}
+
+}
