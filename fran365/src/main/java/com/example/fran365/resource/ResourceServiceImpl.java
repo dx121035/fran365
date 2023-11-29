07f9cee16b6588b3e0860d30756b0c29eb702399
lim <winterziho@gmail.com>
2023-11-29 16:43:02 +0900

    낙관적 락 주석처리

--- fran365/src/main/java/com/example/fran365/resource/ResourceServiceImpl.java
+++ fran365/src/main/java/com/example/fran365/resource/ResourceServiceImpl.java
@@ -3,6 +3,8 @@ package com.example.fran365.resource;
 
 import com.example.fran365.member.Member;
 import com.example.fran365.member.MemberService;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
+import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -12,6 +14,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -98,7 +101,59 @@ public class ResourceServiceImpl implements ResourceService {
             resourceRepository.delete(resource);
         }
     }
+
+//    @Transactional
+//    public void updateProductStock(Integer id, int amount) {
+//        Optional<Resource> optionalResource = resourceRepository.findById(id);
+//
+//        if (optionalResource.isPresent()) {
+//            Resource resource = optionalResource.get();
+//
+//            try {
+//                resource.setAmount(resource.getAmount() - amount);
+//
+//                resourceRepository.saveAndFlush(resource);
+//
+//                if (resource.getAmount() <= 0) {
+//                    resourceRepository.delete(resource);
+//                }
+//            } catch (ObjectOptimisticLockingFailureException e) {
+//                System.out.println("동시성 이슈 발생!!!");
+//                handleVersionConflict(resource, int amount)
+//
+//                // 롤백을 수행하기 위해 트랜잭션을 수동으로 롤백 상태로 설정
+//
+//
+//
+//            }
+//        } else {
+//            throw new RuntimeException("재고가 없습니다.");
+//        }
+//    }
+//@Transactional
+//public void handleVersionConflict(Resource resource, int amount) {
+//    try {
+//        // 버전 충돌이 발생한 경우, 새로운 버전의 Resource를 불러와서 amount를 갱신
+//        Resource refreshedResource = resourceRepository.findById(resource.getId())
+//                .orElseThrow(() -> new RuntimeException("Resource not found after version conflict"));
+//
+//        // 갱신된 amount를 적용
+//        refreshedResource.setAmount(refreshedResource.getAmount() - amount);
+//
+//        // 재고가 음수가 되면 예외 발생
+//        if (refreshedResource.getAmount() < 0) {
+//            throw new VersionConflict("재고가 부족합니다.");
+//        }
+//
+//        // 갱신된 Resource 저장
+//        resourceRepository.save(refreshedResource);
+//    } catch (Exception e) {
+//        // 예외 처리
+//        e.printStackTrace();
+//        // 롤백이 발생하지 않도록 처리
+//    }
 }
+
 
 
 
