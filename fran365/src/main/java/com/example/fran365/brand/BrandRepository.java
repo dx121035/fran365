695890b32ddfe6b81099e13a727d00146f2078ee
kimsunghee33 <137142654+kimsunghee33@users.noreply.github.com>
2023-11-29 15:44:09 +0900

    brand_11/29

---
+++ fran365/src/main/java/com/example/fran365/brand/BrandRepository.java
@@ -0,0 +1,11 @@
+/*name:sunghee kim
+date:2023/11/23
+mail: inew3w@gmail.com
+*/
+
+package com.example.fran365.brand;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BrandRepository extends JpaRepository<Brand, Integer> {
+}
