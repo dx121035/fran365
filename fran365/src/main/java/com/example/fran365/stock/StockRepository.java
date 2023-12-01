fb6ce485667bf9c87634619701f5bf5c4a336af1
lim <winterziho@gmail.com>
2023-12-01 14:07:02 +0900

    stock entity를 추가하여 직영점의 재고관리, 재고 공유 구매 or 판매 시 직영점 재고 증가, 감소

---
+++ fran365/src/main/java/com/example/fran365/stock/StockRepository.java
@@ -0,0 +1,7 @@
+package com.example.fran365.stock;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StockRepository extends JpaRepository<Stock, Integer> {
+
+}
---
+++ fran365/src/main/java/com/example/fran365/stock/StockService.java
@@ -0,0 +1,6 @@
+package com.example.fran365.stock;
+
+public interface StockService {
+
+    void trade(Integer id, int amount, String category);
+}
