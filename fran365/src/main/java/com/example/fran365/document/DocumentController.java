07946bf6ac2d627ae5a011758683ae9ee0d87f3c
lee <yghygh618@gmail.com>
2023-12-11 16:40:28 +0900

    update
    update

--- fran365/src/main/java/com/example/fran365/document/DocumentController.java
+++ fran365/src/main/java/com/example/fran365/document/DocumentController.java
@@ -8,6 +8,7 @@ import com.example.fran365.member.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
