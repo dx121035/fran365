a522a858133df4b8f9a3b6d572af855692351ca6
seulaseulaa <leeseulaseula@gmail.com>
2023-12-14 17:30:30 +0900

    소셜쿼리수정

--- fran365/src/main/java/com/example/fran365/social/SocialRepository.java
+++ fran365/src/main/java/com/example/fran365/social/SocialRepository.java
@@ -10,9 +10,9 @@ import java.util.List;
 
 public interface SocialRepository extends JpaRepository<Social,Integer> {
     List<Social> findByUsernameOrderByCreateDateDesc(String username);
-    @Query("SELECT s FROM Social s WHERE s.username = :username OR s.status = '1' ORDER BY s.createDate DESC")
-    List<Social> findPublicPostsOrByUsername(String username);
-
+    //@Query("SELECT s FROM Social s WHERE s.username = :username OR s.status = '1' ORDER BY s.createDate DESC")
+    //List<Social> findPublicPostsOrByUsername(String username);
+    @Query("SELECT s FROM Social s WHERE s.status = '1' ORDER BY s.createDate DESC")
     Page<Social> findPageBy(Pageable pageable);
 
 
